341b39d0694d364a08d5b515769dd377d8b08c6e
Jacob Briggs <jdbriggs@sep.com>
2021-01-29 09:20:22 -0600

    integrated db relationship topics and blogs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_162804) do
+ActiveRecord::Schema.define(version: 2021_01_29_151210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,9 @@ ActiveRecord::Schema.define(version: 2021_01_26_162804) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "slug"
     t.integer "status", default: 0
+    t.bigint "topic_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
+    t.index ["topic_id"], name: "index_blogs_on_topic_id"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
@@ -57,4 +59,11 @@ ActiveRecord::Schema.define(version: 2021_01_26_162804) do
     t.index ["slug"], name: "index_skills_on_slug", unique: true
   end
 
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "blogs", "topics"
 end
